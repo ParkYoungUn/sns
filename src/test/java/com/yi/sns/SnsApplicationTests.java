68f8802237dc029703ff5da8d8a0bca982f6490f
ParkYoungUn <youngun45@naver.com>
2020-09-25 17:10:53 +0900

    2020/09/25 17:10

---
+++ src/test/java/com/yi/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yi.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
